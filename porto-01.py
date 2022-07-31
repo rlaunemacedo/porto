8afc9addd29ec07579b80879130a4cf82ca7fafb
rlaunemacedo <72508944+rlaunemacedo@users.noreply.github.com>
2022-07-31 16:38:38 -0300

    Update porto-01.py

--- porto-01.py
+++ porto-01.py
@@ -24,14 +24,23 @@ st.sidebar.subheader('Dicas:')
 st.sidebar.write('1. Para ver um ativo em particular, na legenda, dê um clique duplo;')
 st.sidebar.write('2. Para esconder um ativo em particular, na legenda, dê um clique simples;')
 
-st.header('Porto de Itapoá')
-st.subheader('Demonstrativo Financeiro (em milhares de reais - R$)')
+st.markdown("<h4 style='text-align: center; color: blue;'>Porto de Itapoá</h1>", unsafe_allow_html=True)
+st.markdown("<h6 style='text-align: center; color: black;'>Demonstrativo Financeiro - Período de 2012 a 2020</h1>", unsafe_allow_html=True)
 
 dfcirc = df[df['Circulante'] == 'Ativo circulante']
 dfncirc = df[df['Circulante'] != 'Ativo circulante']
 
-fig1 = px.line(dfcirc, x="Ano", y='Valor',hover_data={"Ano"}, 
-               color='Ativo',line_shape="spline")
+#fig = px.scatter(df, x="sepal_length", y="sepal_width", color="species",
+#                 labels={
+#                     "sepal_length": "Sepal Length (cm)",
+#                     "sepal_width": "Sepal Width (cm)",
+#                     "species": "Species of Iris"
+#                 },
+#                title="Manually Specified Labels")
+
+fig1 = px.line(dfcirc, x="Ano", y='Valor',hover_data={"Ano"},color='Ativo',line_shape="spline",labels={
+                     "Valor": "Valor (x1000)",
+                     "Ativo": "Ativos"})
 fig1.update_xaxes(dtick=1)
 fig1.update_layout(title='Circulante')
 
